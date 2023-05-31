78ea34489a335b724582333c396271a3d853105f
Oleksii Leshchenko <84497187+alexbreamdev@users.noreply.github.com>
2023-05-31 10:00:45 +0300

    Color of link changed in main

--- GitDemo/ContentView.swift
+++ GitDemo/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
             NavigationLink("To modal View", destination: {
                 ModalView()
             })
+            .foregroundColor(.red)
             .navigationTitle("First View")
         }
         .padding()
